2819c8a3c92b35c51ebea5e766c87262c175df26
dbaldew <dbaldew@gmail.com>
2021-10-06 11:21:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 
+//fetch data
+//home page loads first 20
+
+
 function App() {
   return (
+
+//button previous 20 (disabled at 0) - button next 20 (disabled at max )
+//loading message while fetching data
+//card: name, sprite, nr of moves, weight, list of abilities
+
+
     <div>
-      Begin hier met de opdracht!
+        <div className="cardContainer">
+            <h3>name</h3>
+            <h3>image</h3>
+            <h3>moves</h3>
+            <h3>abilities</h3>
+            <p>ability</p>
+            <p>ability</p>
+            <p>ability</p>
+        </div>
     </div>
   );
 }
