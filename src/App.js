2194d49baac8a06f449822d85b9dfe22fd31382b
dbaldew <dbaldew@gmail.com>
2021-10-06 19:29:20 +0200

    card

--- src/App.js
+++ src/App.js
@@ -1,87 +1,22 @@
 import React from 'react';
-import axios from 'axios';
-import {useState, useEffect} from "react";
+//import axios from 'axios';
+//import {useState, useEffect} from "react";
 import './App.css';
 
-//import Card from "./Components/Card/Card";
-
+import Card from "./Components/Card/Card";
 
 function App() {
     //state
-    const endpoint = `https://pokeapi.co/api/v2/pokemon/jigglypuff`
-    const [pokemon, setPokemon] = useState({});
-
-    //fetch data
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/jigglypuff`);
-                console.log(result.data);
-                setPokemon(result.data)
-            } catch (e) {
-                console.error(e);
-            }
-
-        }fetchData()
-    },[endpoint]
-    )
 
 
-    //home page loads first 20
+    //fetch all pokemon - home page loads first 20
+    //loading message while fetching data
+    //button previous 20 (disabled at 0) - button next 20 (disabled at max )
+
     return (
+        <Card
 
-//button previous 20 (disabled at 0) - button next 20 (disabled at max )
-//loading message while fetching data
-//card: name, sprite, nr of moves, weight, list of abilities
-
-
-
-
-            <div className="cardContainer">
-                {Object.keys(pokemon).length > 0 &&
-                <>
-                <h3>{pokemon.name}</h3>
-                {<img src={pokemon.sprites.front_default}/>}
-                    <ul>
-                        {pokemon.abilties.map((ability) => {
-                            return(
-                                <li key={pokemon.name}>
-                                    {ability.ability.name}
-                                </li>)
-                        })}
-                    </ul>
-
-                {/* alt="front default"*/}
-                {/*/>*/}
-                {/*    <h3>moves</h3>*/}
-                {/*{pokemon.abilties.map((move) => {*/}
-                {/*    return(*/}
-                {/*    <li key={pokemon.name}>*/}
-                {/*{move.ability.name}*/}
-                {/*    </li>)*/}
-
-                })}
-                </>
-                }
-
-                    {/*{<img/>}*/}
-
-                    {/* alt="front default"*/}
-                    {/*/>*/}
-                {/*    <h3>moves</h3>*/}
-                {/*{pokemon.abilties.map((move) => {*/}
-                {/*    return(*/}
-                {/*    <li key={pokemon.name}>*/}
-                {/*        {move.ability.name}*/}
-                {/*    </li>)*/}
-                {/*})}*/}
-                    {/*{pokemon['abilities'].map((ability)=>{*/}
-                    {/*    return(*/}
-                    {/*        <p key={ability.slot}>{pokemon['abilities']}</p>*/}
-                    {/*    )*/}
-                    {/*    }*/}
-                    {/*)}*/}
-            </div>
+        />
 
     );
 }
--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Content.css'
 
+
+
 function Content() {
     return (
         <div>
