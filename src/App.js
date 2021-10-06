a4c7f345f055fe459efa6422914be6b7412c2459
dbaldew <dbaldew@gmail.com>
2021-10-06 22:40:22 +0200

    useEffect getAllPoke

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 //import {useState, useEffect} from "react";
 import './App.css';
@@ -6,20 +6,27 @@ import './App.css';
 import Card from "./Components/Card/Card";
 
 function App() {
+
     //state
     const [allPoke, setAllPoke] = useState({});
 
-    //fetch all pokemon - home page loads first 20
-    async function getAllPoke() {
-        try {
-            const result = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20');
-            console.log(result.data)
-            setAllPoke(result.data)
-        } catch (e) {
-            console.error(e)
-        }
-    };
-    getAllPoke();
+    //useEffect
+    useEffect(()=> {
+
+        //fetch all pokemon - home page loads first 20
+        async function getAllPoke() {
+            try {
+                const result = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20');
+                console.log(result.data)
+                setAllPoke(result.data)
+            } catch (e) {
+                console.error(e)
+            }
+        };
+        getAllPoke();
+
+    }, []);
+
 
 
     //loading message while fetching data
